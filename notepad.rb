315299322b5efda7ce8a39b66d351638ab168076
Kirill Nosenko <mensork@yandex.ru>
2017-10-03 11:50:15 +0300

    notepad working version 1.0

--- notepad.rb
+++ notepad.rb
@@ -12,3 +12,25 @@ require_relative 'task.rb'
 require_relative 'memo.rb'
 require_relative 'link.rb'
 
+puts "Привет, я твой блокнот!"
+
+puts "Что хотите записать в блокнот?"
+
+choices = Post.post_types
+
+choice = -1
+
+until choice >= 0 && choice < choices.size
+  choices.each_with_index do |type, index|
+    puts "\t#{index}. #{type}"
+  end
+  choice = gets.chomp.to_i
+end
+
+entry = Post.create(choice)
+
+entry.read_from_console
+
+entry.save
+
+puts "Ваша запись сохранена!"
